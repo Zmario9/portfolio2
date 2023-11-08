d49409292e2593f58ea217467ad4a13b0c8c4b66
Jose Manuel <jcorreacastro99@gmail.com>
2023-11-08 18:25:36 -0400

    Copia por si algo sale mal probando displays

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  public hamburguerOpen: boolean = false;
+  public shouldBeOpened: boolean = true;
+  openHambMenu(event: string){
+    if(!this.hamburguerOpen) {
+      $(event).animate({
+        right: 0 
+      },{
+        start: ()=>{
+          this.shouldBeOpened = false
+        },
+        complete: ()=>{
+          this.shouldBeOpened = true
+        }
+      })
+      this.hamburguerOpen = true;
+    } else {
+      $(event).animate({
+        right: "100vw"
+      },{
+        start: ()=>{
+          this.shouldBeOpened = false
+        },
+        complete: ()=>{
+          this.shouldBeOpened = true
+        }
+      });
+      this.hamburguerOpen = false;     
+    }
+  } 
 }
 
