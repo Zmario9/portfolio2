ac6e4647e06178a586698d4fefb492eb06a8c630
Jose Manuel <jcorreacastro99@gmail.com>
2023-11-09 20:03:21 -0400

    Creacion de responsive de tablets y mobiles, procede a hacer estos responsivos a la hora de cambiarel width de la pantalla

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import * as $ from 'jquery';
 
+
+window.addEventListener("resize", (event) => {
+  console.log("Width cambiando");
+  if(window.innerWidth < 600){
+
+  }
+});
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,32 +17,77 @@ import * as $ from 'jquery';
 export class HeaderComponent {
   public hamburguerOpen: boolean = false;
   public shouldBeOpened: boolean = true;
-  openHambMenu(event: string){
-    if(!this.hamburguerOpen) {
-      $(event).animate({
-        right: 0 
-      },{
-        start: ()=>{
-          this.shouldBeOpened = false
-        },
-        complete: ()=>{
-          this.shouldBeOpened = true
-        }
-      })
-      this.hamburguerOpen = true;
+  openHambMenu(event: string) {
+    console.log(window.innerWidth);
+    if (isMobile()) {
+      if(window.innerWidth <= 600){
+        //MOBILES
+        if (!this.hamburguerOpen) {
+          $('body').css('overflow', 'hidden');
+          $(event).animate({
+            right: 0
+          }, {
+            start: () => {
+              this.shouldBeOpened = false
+            },
+            complete: () => {
+              this.shouldBeOpened = true
+            }
+          })
+          this.hamburguerOpen = true;
+        } else {
+          $('body').css('overflow', 'auto');
+          $(event).animate({
+            right: "100vw"
+          }, {
+            start: () => {
+              this.shouldBeOpened = false
+            },
+            complete: () => {
+              this.shouldBeOpened = true
+            }
+          });
+          this.hamburguerOpen = false;
+        } 
+      } else {
+        //TABLETS
+        if (!this.hamburguerOpen) {
+          $('body').css('overflow', 'hidden');
+          $(event).animate({
+            top: "65vh"
+          }, {
+            start: () => {
+              this.shouldBeOpened = false
+            },
+            complete: () => {
+              this.shouldBeOpened = true
+            }
+          })
+          this.hamburguerOpen = true;
+        } else {
+          $('body').css('overflow', 'auto');
+          $(event).animate({
+            top: "100vh"
+          }, {
+            start: () => {
+              this.shouldBeOpened = false
+            },
+            complete: () => {
+              this.shouldBeOpened = true
+            }
+          });
+          this.hamburguerOpen = false;
+        } 
+      }
     } else {
-      $(event).animate({
-        right: "100vw"
-      },{
-        start: ()=>{
-          this.shouldBeOpened = false
-        },
-        complete: ()=>{
-          this.shouldBeOpened = true
-        }
-      });
-      this.hamburguerOpen = false;     
+      console.log("Desktop device detected");
     }
-  } 
+  }
 }
+
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i;
+  return regex.test(navigator.userAgent);
+}
+
 
