25669eadc211af04b986ac524358c27e2a39aad3
Jose Manuel <jcorreacastro99@gmail.com>
2024-02-23 13:51:02 -0400

    Pongo toda la data en un modulo de scripts ajeno a los componentes para que estos puedan ser accedidos desde cualquier componente

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,19 +3,9 @@ import { ModalComponent } from '../modal/modal.component';
 import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
+//Con esto llamo al script data.ts del modulo de page-data del proyecto.
+import * as myData from '../page-data/page-data.module';
 declare var Swal: any;
-interface NameSrc{
-  nombre: string;
-  src: string;
-}
-
-interface ProjectData{
-  titulo: string;
-  technologies: Array<string>;
-  description: string;
-  src: string;
-  gifSrc: string;
-}
 
 @Component({
   selector: 'app-main',
@@ -24,132 +14,12 @@ interface ProjectData{
 })
 export class MainComponent {
   modalRef: MdbModalRef<ModalComponent> | null = null;
-  conocimientos: NameSrc[] = [
-    {
-      nombre:'HTML5',
-      src:"assets/images/html.png"
-    },
-    {
-      nombre:'JavaScript',
-      src:'assets/images/javascript.png'
-    },
-    {
-      nombre:'CSS',
-      src:'assets/images/css.png'
-    },
-    {
-      nombre:'BootStrap',
-      src:'assets/images/bootstrap.png'
-    },
-    {
-      nombre:'Git',
-      src:'assets/images/git.png'
-    },
-    {
-      nombre:'Jquery',
-      src:'assets/images/jquery.png'
-    },
-    {
-      nombre:'TypeScript',
-      src:'assets/images/typescript.png',
-    },
-    {
-      nombre:'Ionic',
-      src:'assets/images/ionic.png',
-    },
-    {
-      nombre:'Electron',
-      src:'assets/images/electron.png'
-    },
-    {
-      nombre:'Angular',
-      src:'assets/images/angular.png'
-    },
-    {
-      nombre:'NodeJS',
-      src:'assets/images/nodejs.png'
-    },
-    {
-      nombre:'Sass',
-      src:'assets/images/sass.png'
-    }
-  ];
+  conocimientos: myData.NameSrc[] = myData.conocimientos;
+  projects: myData.ProjectData[] = myData.projects;
 
-  projects: ProjectData[] = [
-    {
-      titulo: "Building a House",
-      technologies:[
-        "SCSS", "HTML", "CSS"
-      ],
-      description: `Un modelo basado en el website de una constructora cuyo objetivo es mantenerte 
-                    informado de cómo avanza la construcción de tu hogar, esquema de trabajo, documentos 
-                    legales, entre otras cosas.`,
-      src: "assets/images/proyectos/buildHouse.PNG",
-      gifSrc:"assets/gifs/BuildingAHouse.gif"
-    },
-    {
-      titulo: "Natural Foods",
-      technologies:[
-        "SCSS", "HTML", "CSS"
-      ],
-      description: `Un modelo de página web de recetas sobre comidas naturales, el cual posee una simple interfaz
-                    Bien decorada con su texto descriptivo, mas botones que te llevan dichas recetas que desees aprender.`,
-      src: "assets/images/proyectos/naturalFoods.PNG",
-      gifSrc:"assets/gifs/NaturalFoods.gif"
-    },
-    {
-      titulo: "Education Page",
-      technologies:[
-        "SCSS", "HTML", "CSS"
-      ],
-      description: `Un modelo de website sobre educación para niños, lleno de bastante estilo que invita invita a los 
-                    padres a ser mas responsables y amables con la crianza y educacion de sus hijos.`,
-      src: "assets/images/proyectos/EducationPage2.PNG",
-      gifSrc:"assets/gifs/EducationBoring.gif"
-    },
-    {
-      titulo: "Prototipo tienda de ventas",
-      technologies:[
-        "SCSS", "HTML", "CSS", "BOOTSTRAP", "ANGULAR"
-      ],
-      description: `Un modelo de website que alude a ser un clon de Mercadolibre, al hacer click a un producto para comprar
-                    te llevara a la seccion donde se describe el precio especifico y demas del mismo.`,
-      src: "assets/images/proyectos/ClonePrototypeShop.PNG",
-      gifSrc:"assets/gifs/MercadoLibreClone.gif"
-    },
-    {
-      titulo: "Agencia de Marketing",
-      technologies:[
-        "CSS", "HTML"
-      ],
-      description:`Pagina web sobre Marketing Digital, con animaciones smooth, diseño minimalista con colores llenos
-                   de vida agradables para la vista, botones con hover animado y buena organización del contenido.`,
-      src: "assets/images/proyectos/AgentesWeb.PNG",
-      gifSrc:"assets/gifs/AgenciaOK.gif"
-    },
-    {
-      titulo: "Portafolio simple",
-      technologies:[
-        "CSS", "HTML", "Responsive Design"
-      ],
-      description: `Portafolio hecho bajo un modelo dado en Cadif1, cuyo propósito era ponernos a prueba sobre cómo
-                    Decorar y organizar correctamente el FrontPage, con animaciones de hover.`,
-      src: "assets/images/proyectos/Portfolio1Template.PNG",
-      gifSrc:"assets/gifs/PortfolioModel.gif"
-    },
-    {
-      titulo: "Modelo 2 de Portafolio",
-      technologies:[
-        "CSS", "HTML", "Responsive Design"
-      ],
-      description: `Mi primer modelo de portafolio hecho bajo un modelo dado en Cadif1, decoración minimalista
-                    con una simple paleta de colores, texto amigable para la vista, y un buen ajuste de pantalla.`,
-      src:"assets/images/proyectos/PortfolioTemplate3.PNG",
-      gifSrc:"assets/gifs/PortfolioModel2.gif"
-    }
-  ];
   constructor(private modalService: MdbModalService, private router: Router) {}
   ngOnInit(){
+    // console.log(myData.dataAlgo);
     if(window.innerWidth > 1200){
       $("main").css("margin-top", `${$("header").height()}px`);
     } else {
---
+++ src/app/page-data/page-data.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+export * from './data';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ]
+})
+export class PageDataModule { }
